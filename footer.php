c3c8941d95007c4a2e6ed2e8db8e79e30c9c6536
Vincent NGUYEN <64532628+vngn@users.noreply.github.com>
2021-06-27 14:55:07 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<p>Site (en cours de développement) réalisé par :</p>
+<p><a class="agence" href="https://vincent-nguyen.developpeur-de-site-web.fr/" target="_blank">Vincent NGUYEN</a>,
+    développeur web<br><br>
+    (c) Copyright 2021<br><br>
+    Reproduction totale ou partielle interdite sans l'autorisation de l'auteur</p>
